153ab4702a3f3867111b79d3fb509f9862587a7c
Vladimir <euroflock@hotmail.com>
2019-02-11 21:56:31 +0200

    8 scale 0.5

--- js/bundle.js
+++ js/bundle.js
@@ -87,7 +87,7 @@
 
 	function onWindowScroll () {
 		windowWidth = $window.width();
-		windowHeight = $window.height();
+		windowHeight = 850; // $window.height();
 		rootScrollTop = $('html')[0].scrollTop || $('body')[0].scrollTop || 0;
 		section1Top = $section1[0].offsetTop - rootScrollTop;
 		section2Top = $section2[0].offsetTop - rootScrollTop;
